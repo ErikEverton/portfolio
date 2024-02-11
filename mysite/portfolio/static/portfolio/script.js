b84909060b9c5f763dadd3191fd417d48a095eed
Erik Everton Rodrigues <erik.everton444@gmail.com>
2024-02-10 21:19:43 -0300

    header done i think

--- mysite/portfolio/static/portfolio/script.js
+++ mysite/portfolio/static/portfolio/script.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector(".hamburguer");
+const menu = document.querySelector(".menu")
 
 hamburger.addEventListener("click", () => {
-    console.log("click");
     if (hamburger.classList.contains("open")) {
         hamburger.classList.remove("open");
+        menu.classList.add("disable")
     } else {
         hamburger.classList.add("open");
+        menu.classList.remove("disable")
     }
 });
 
